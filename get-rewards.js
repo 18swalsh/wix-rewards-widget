253101f10aee638438aebd052ba72b1a21efe454
18swalsh <18swalsh@gmail.com>
2020-04-09 16:08:59 -0400

    Add files via upload

--- get-rewards.js
+++ get-rewards.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    console.log("rewards-display")
+    var promoCode = "TEST1";
+    var rewardsPoints = "10000";
+
+    $("#promoCode").text(promoCode);
+    $("#rewardsPoints").text(rewardsPoints);
+    
 })
